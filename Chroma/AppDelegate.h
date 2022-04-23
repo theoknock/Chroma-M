1b3e2846fa67a8791a36353a0c7f97e18a65ddc2
James Alan Bush <jamesalanbush1972@gmail.com>
2022-04-23 17:39:53 -0400

    Initial Commit

---
+++ Chroma/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Chroma
+//
+//  Created by Xcode Developer on 4/23/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chroma/Renderer.h
@@ -0,0 +1,17 @@
+//
+//  Renderer.h
+//  Chroma
+//
+//  Created by Xcode Developer on 4/23/22.
+//
+
+#import <MetalKit/MetalKit.h>
+
+// Our platform independent renderer class.   Implements the MTKViewDelegate protocol which
+//   allows it to accept per-frame update and drawable resize callbacks.
+@interface Renderer : NSObject <MTKViewDelegate>
+
+-(nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)view;
+
+@end
+
